536ef44a453a571f8ef3efb6287d8b5b38667560
junsuPark <krpjs0508@gmail.com>
2023-01-25 14:27:31 +0900

    initial commit

---
+++ issue-management-spring-boot-application/src/main/kotlin/app/junsu/issuemanagementspringbootapplication/IssueManagementSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package app.junsu.issuemanagementspringbootapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssueManagementSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<IssueManagementSpringBootApplication>(*args)
+}
