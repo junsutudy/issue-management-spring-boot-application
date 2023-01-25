94cf8edf0f978ed91b65deb63f40c8732318d479
junsuPark <krpjs0508@gmail.com>
2023-01-25 16:49:28 +0900

    enable JPA auditing on application class

--- issue-management-spring-boot-application/issue-service/src/main/kotlin/app/junsu/issuemanagementspringbootapplication/IssueManagementSpringBootApplication.kt
+++ issue-management-spring-boot-application/issue-service/src/main/kotlin/app/junsu/issuemanagementspringbootapplication/IssueManagementSpringBootApplication.kt
@@ -2,8 +2,10 @@ package app.junsu.issuemanagementspringbootapplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class IssueManagementSpringBootApplication
 
 fun main(args: Array<String>) {
