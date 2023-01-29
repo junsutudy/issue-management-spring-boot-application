a351b70ed53ccb9a586d70480b9701daca20206f
junsuPark <krpjs0508@gmail.com>
2023-01-29 13:24:41 +0900

    user module

--- issue-management-spring-boot-application/build.gradle.kts
+++ issue-management-spring-boot-application/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
+    kotlin("kapt") version "1.7.22"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
--- issue-management-spring-boot-application/settings.gradle.kts
+++ issue-management-spring-boot-application/settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "issue-management-spring-boot-application"
 include("issue-service")
+include("user-service")
